c444ba338468297ce877e061a13528c313ab49b9
Lempi-sudo <andreii.lapschow1999@mail.ru>
2021-12-16 21:10:42 +0400

    Cделал загрузку изображений в виде numpy массивов из директрии

---
+++ ImageWork.py
@@ -0,0 +1,32 @@
+import os
+from skimage.io import imread
+
+
+class LoadNamesImage:
+    def __init__(self):
+        self.countImage = 0
+
+    def get_list_image_name(self, directory_name: str):
+        image_name_list = []
+        for dirpath, dirnames, filenames in os.walk(directory_name):
+            for filename in filenames:
+                path = dirpath + '/' + filename
+                image_name_list.append(path)
+                self.countImage += 1
+            return image_name_list
+
+
+class LoadImage:
+    def __init__(self, name_image_list: list):
+        self.name_image_list = name_image_list
+        self.cont_image = len(name_image_list)
+        self.iterListImage = iter(name_image_list)
+
+    def next_image(self):
+        if self.cont_image > 0:
+            name_path_image = next(self.iterListImage)
+            image_numpy = imread(name_path_image)
+            self.cont_image -= 1
+        else:
+            raise StopIteration
+        return image_numpy
--- main.py
+++ main.py
@@ -1,5 +1,47 @@
+import os
+from PIL import Image
+from ImageWork import LoadNamesImage, LoadImage
+from matplotlib import pyplot as plt
+from skimage.io import imread
+import numpy as np
 
+
+# def test():
+#     listdir = os.listdir()
+#
+#     tmpdir = os.getcwd()
+#
+#     for dirpath, dirnames, filenames in os.walk("TestDataSet"):
+#         # перебрать каталоги
+#         for dirname in dirnames:
+#             print("Каталог:", os.path.join(dirpath, dirname))
+#         # перебрать файлы
+#         for filename in filenames:
+#             path = dirpath + '/' + filename
+#             embed_image = imread(path)
+#             img = Image.fromarray(embed_image.astype(np.uint8))
+#             Image_RGB_np2 = np.asarray(img)
+#             image = Image.open(path)
+#             Image_RGB_np = np.asarray(image)
+#             image.show()
+#             plt.imshow(image, cmap='grey')
+#             plt.show()
+#             print("Файл:", os.path.join(dirpath, filename))
+#
+#     print("Все папки и файлы:", os.listdir())
 
 
 if __name__ == '__main__':
-    pass
+    load_name = LoadNamesImage()
+    path_name_image = load_name.get_list_image_name('TestDataSet')
+    load_image = LoadImage(path_name_image)
+
+    try:
+        while True:
+            image = load_image.next_image()
+            print(image)
+
+    except StopIteration:
+        print("все изображения считаны")
+
+    print()
