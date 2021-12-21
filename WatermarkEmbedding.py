acfeec0cd79116939f766fc993e354c098fdfe50
Lempi-sudo <andreii.lapschow1999@mail.ru>
2021-12-22 00:27:59 +0400

    рефакторинг

--- WatermarkEmbedding.py
+++ WatermarkEmbedding.py
@@ -1,24 +1,16 @@
 import numpy as np
 
-    class GenerateBinaryWaterMark:
-        def water_mark(self, size=32*32):
-            return np.random.randint(2, size=size)
+
+class GenerateBinaryWaterMark:
+    @staticmethod
+    def water_mark(size=32 * 32):
+        return np.random.randint(2, size=size)
 
 
-    class WatermarkEmbedding():
+class WatermarkEmbedding:
+    def __init__(self, f, w):
+        self.W = w
+        self.f = f
 
-        def __init__(self ,f, W ):
-            self.W=W
-            self.f=f
-
-
-        def embed(self):
-            pass
-
-
-
-
-
-
-
-
+    def embed(self):
+        pass
