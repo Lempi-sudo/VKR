1ba38974156f2f5a0e1da5949f3ffdb7cd884766
Lempi-sudo <andreii.lapschow1999@mail.ru>
2021-12-22 09:05:20 +0400

    Выделил область hl2

--- WatermarkEmbedding.py
+++ WatermarkEmbedding.py
@@ -8,9 +8,32 @@ class GenerateBinaryWaterMark:
 
 
 class WatermarkEmbedding:
-    def __init__(self, f, w):
+    def __init__(self, w):
         self.W = w
-        self.f = f
 
-    def embed(self):
-        pass
+
+    def __extract_hl2_area__(self ,f):
+        Nf=f.shape[0]
+        lh1=f[Nf//2:Nf,0:Nf//2]
+
+        Nlh1=lh1.shape[0]
+
+        hl2=lh1[0:Nlh1//2,Nlh1//2:Nlh1]
+        return hl2
+
+
+    def embed(self,f):
+        if f.shape[0]>64 or f.shape[1]>64:
+            hl2 = self.__extract_hl2_area__(f)
+
+        print(f)
+
+
+
+if __name__ == '__main__':
+    mat=np.random.randint(0, 5, size=(256,256))
+
+    emb= WatermarkEmbedding(GenerateBinaryWaterMark.water_mark())
+
+    emb.embed(mat)
+
