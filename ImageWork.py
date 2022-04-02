c19640b40489342ce87a15b5a83854a821663b5f
Lempi-sudo <andreii.lapschow1999@mail.ru>
2022-04-03 03:30:27 +0400

    Рефакторинг Ренейминг

--- ImageWork.py
+++ ImageWork.py
@@ -1,7 +1,6 @@
 import os
 from skimage.io import imread
-from  ExceptionsModule import WaterMarkWrong
-
+from ExceptionsModule import WaterMarkWrong
 
 
 class ImagesNamesLoader:
@@ -18,16 +17,15 @@ class ImagesNamesLoader:
             return image_name_list
 
 
-
 class ImageLoader:
     def __init__(self, name_image_list: list):
         self.name_image_list = name_image_list
         self.cont_image = len(name_image_list)
-        self.iterListImage = iter(name_image_list)
+        self.name_images_iterator = iter(name_image_list)
 
     def next_image(self):
         if self.cont_image > 0:
-            name_path_image = next(self.iterListImage)
+            name_path_image = next(self.name_images_iterator)
             image_numpy = imread(name_path_image)
             self.cont_image -= 1
         else:
@@ -35,23 +33,19 @@ class ImageLoader:
         return image_numpy
 
 
-
 class WaterMarkLoader():
     @staticmethod
-    def load(path , treshold=100 , right_size_watermark = 32):
+    def load(path, treshold=100, right_size_watermark=32):
         try:
             water_mark = imread(path)
             if water_mark.shape[0] != right_size_watermark or water_mark.shape[1] != right_size_watermark:
-                raise WaterMarkWrong("Неправильный  водяной знак", right_size_watermark , water_mark.shape[0], water_mark.shape[1])
+                raise WaterMarkWrong("Неправильный  водяной знак", right_size_watermark, water_mark.shape[0],
+                                     water_mark.shape[1])
             if water_mark.shape[2] != 1:
-                water_mark=water_mark[:,:,0]
+                water_mark = water_mark[:, :, 0]
                 water_mark[water_mark < treshold] = 0
                 water_mark[water_mark >= treshold] = 1
         except WaterMarkWrong as e:
             print(e)
             raise SystemExit(1)
         return water_mark
-
-
-
-
